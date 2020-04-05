d279899cd850100ec737349171a3ae5bee341ff7
MichaelKurzak <55596437+MichaelKurzak@users.noreply.github.com>
2020-04-05 20:15:33 +0200

    add memory father. add new class "Person"

--- src/main/java/a_Zadania/a_Dzien_1/d_Konstruktory/Calculator.java
+++ src/main/java/a_Zadania/a_Dzien_1/d_Konstruktory/Calculator.java
@@ -1,36 +1,39 @@
 package a_Zadania.a_Dzien_1.d_Konstruktory;
 
 public class Calculator {
-    public static String score;
-    double result;
-    String free;
-    private String[] historyOfOperation;
 
+    String[] historyOfOperation;
+    int operation;
+    double result;
 
     public Calculator() {
-        historyOfOperation = new String[10];
-        for (int i = 0; i < historyOfOperation.length; i++)
-            historyOfOperation[i] = score;
+        historyOfOperation = new String[0];
 
     }
 
-
-    public String add(double num1, double num2) {
+    public void add(double num1, double num2) {
         result = num1 + num2;
-        score = "Add " + num1 + " to " + num2 + " got " + result;
-        return score;
+        if (historyOfOperation[operation] == null) {
+            historyOfOperation[operation] = "Added " + num1 + " to " + num2 + " got " + result;
+            operation++;
+        }
+
     }
 
     public void multiply(double num1, double num2) {
-        System.out.println(result = num1 * num2);
-        score = "Multiplied " + num1 + " with " + num2 + " got " + result;
-
+        result = num1 * num2;
+        if (historyOfOperation[operation] == null) {
+            historyOfOperation[operation] = "Multiplied " + num1 + " with " + num2 + " got " + result;
+            operation++;
+        }
     }
 
     public void subtract(double num1, double num2) {
-        System.out.println(result = num1 - num2);
-        score = "Subtracted " + num1 + " from " + num2 + " got " + result;
-
+        result = num1 - num2;
+        if (historyOfOperation[operation] == null) {
+            historyOfOperation[operation] = "Subtracted " + num1 + " from " + num2 + " got " + result;
+            operation++;
+        }
     }
 
     public void divided(double num1, double num2) {
@@ -39,14 +42,14 @@ public class Calculator {
         } else {
             result = Integer.parseInt("Nie można dzielić przez '0'");
         }
-        score = "Divided " + num1 + " by " + num2 + " got " + result;
-        System.out.println(score);
+        if (historyOfOperation[operation] == null) {
+            historyOfOperation[operation] = "Divided " + num1 + " by " + num2 + " got " + result;
+            operation++;
+        }
     }
 
-
     public void printOperations() {
-
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < historyOfOperation.length; i++)
             System.out.println(i + " " + historyOfOperation[i]);
     }
 
--- src/main/java/a_Zadania/a_Dzien_1/d_Konstruktory/Main1.java
+++ src/main/java/a_Zadania/a_Dzien_1/d_Konstruktory/Main1.java
@@ -5,7 +5,13 @@ public class Main1 {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.add(1,2);
+        calculator.add(5,6);
+        calculator.divided(10,5);
+        calculator.subtract(20,22);
+        calculator.add(10,10);
+        calculator.multiply(50,20);
+        calculator.add(50,20);
+
         calculator.printOperations();
 
     }
