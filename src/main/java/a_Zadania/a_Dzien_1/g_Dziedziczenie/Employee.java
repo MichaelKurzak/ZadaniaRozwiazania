a3b077fdb4d088b6716d370a6fbb59454da22ff4
MichaelKurzak <55596437+MichaelKurzak@users.noreply.github.com>
2020-06-13 08:32:32 +0200

    add class Employee

--- src/main/java/a_Zadania/a_Dzien_1/g_Dziedziczenie/Employee.java
+++ src/main/java/a_Zadania/a_Dzien_1/g_Dziedziczenie/Employee.java
@@ -2,4 +2,26 @@ package a_Zadania.a_Dzien_1.g_Dziedziczenie;
 
 
 public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double wage;
+
+    public Employee(int id, String firstName, String lastName, double wage) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.wage = wage;
+    }
+
+    public double raiseWage(double percent) {
+        double result = 0;
+
+        if (wage >= 0) {
+            result = wage * percent;
+        }
+        return result;
+    }
+
+
 }
